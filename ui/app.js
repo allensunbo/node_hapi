0ff2dc1efe6f2a90a202445aaca36bdbb1c39b77
axplay0829@gmail.com <axplay0829@gmail.com>
2014-12-18 12:56:05 -0500

    split

--- ui/app.js
+++ ui/app.js
@@ -1,58 +1,74 @@
-var app = angular.module('app', []);
-app.controller('MainCtrl', function($scope, UserService) {
-	$scope.number1 = 2;
-	$scope.number2 = 3;
-});
+(function(angular) {
+	var app = angular.module('app', []);
+	app.controller('MainCtrl', function($scope, UserService) {
+		var vm = this;
+		vm.number1 = 2;
+		vm.number2 = 3;
+	});
 
-app.service('UserService', function($log, $timeout, $q) {
-	var users = [{
-		'name': 'alex'
-	}, {
-		'name': 'john'
-	}, {
-		'name': 'smith'
-	}, {
-		'name': 'zoe'
-	}];
-	return {
-		getAllUsers: function() {
-			$log.info('begin loading users ...');
-			var defer = $q.defer();
-			$timeout(function() {
-				var usersClone = [];
-				// avoid two user-list directives changing the same data set!
-				angular.copy(users, usersClone);
-				defer.resolve(usersClone);
-				$log.info('end loading users ...');
-			}, 1000);
-			return defer.promise;
-		}
-	}
-});
-
-app.directive('userList', function(UserService, $log) {
-	return {
-		restrict: 'E',
-		scope: {
-			number: '=number'
-		},
-		template: '<button ng-click="load()">Load Users</button><ul ng-repeat="user in users"> <li> <input type="text" ng-model="user.name"/> </li></ul>',
-		link: function(scope, element, attributes, controller) {
-			scope.load = function() {
-				UserService.getAllUsers().then(function(data) {
-					// you can avoid the above issue here
-					/*var usersClone = [];
-					angular.copy(data.slice(0, scope.number), usersClone);
-					scope.users = usersClone;*/
-
-					scope.users = data.slice(0, scope.number);
-				}, function(reason) {
-					$log.error(reason);
-				});
+	app.service('UserService', function($log, $timeout, $q) {
+		var users = [{
+			'name': 'alex'
+		}, {
+			'name': 'john'
+		}, {
+			'name': 'smith'
+		}, {
+			'name': 'zoe'
+		}];
+		return {
+			getAllUsers: function() {
+				$log.info('begin loading users ...');
+				var defer = $q.defer();
+				$timeout(function() {
+					var usersClone = [];
+					// avoid two user-list directives changing the same data set!
+					angular.copy(users, usersClone);
+					defer.resolve(usersClone);
+					$log.info('end loading users ...');
+				}, 1000);
+				return defer.promise;
 			}
-		},
-		controller: function($scope) {
-
 		}
-	}
-});
+	});
+
+	app.directive('userList', function(UserService, $log) {
+		return {
+			restrict: 'E',
+			scope: {
+				number: '=number'
+			},
+			templateUrl: 'user-list.html',
+			link: function(scope, element, attributes, controller) {
+				scope.load = function() {
+					UserService.getAllUsers().then(function(data) {
+						// you can also avoid the above issue here
+						/*var usersClone = [];
+						angular.copy(data.slice(0, scope.number), usersClone);
+						scope.users = usersClone;*/
+
+						scope.users = data.slice(0, scope.number);
+					}, function(reason) {
+						$log.error(reason);
+					});
+				};
+				scope.topUser = undefined;
+				scope.setTopUser = function(user) {
+					scope.topUser = user;
+					controller.setTopUser(user);
+					console.log(controller.getTopUser());
+				};
+			},
+			controller: function($scope) {
+				var topUser = null;
+				this.getTopUser = function() {
+					return topUser;
+				}
+
+				this.setTopUser = function(user) {
+					topUser = user;
+				}
+			}
+		}
+	});
+})(angular);
