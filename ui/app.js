a29ccfddc3741c93e0e3210935fd2bc587c7b7e5
axplay0829@gmail.com <axplay0829@gmail.com>
2014-12-18 09:42:56 -0500

    change

--- ui/app.js
+++ ui/app.js
@@ -1,20 +1,28 @@
 var app = angular.module('app', []);
 app.controller('MainCtrl', function($scope, UserService) {
-
-
+	$scope.number1 = 2;
+	$scope.number2 = 3;
 });
 
 app.service('UserService', function($log, $timeout, $q) {
+	var users = [{
+		'name': 'alex'
+	}, {
+		'name': 'john'
+	}, {
+		'name': 'smith'
+	}, {
+		'name': 'zoe'
+	}];
 	return {
 		getAllUsers: function() {
 			$log.info('begin loading users ...');
 			var defer = $q.defer();
 			$timeout(function() {
-				defer.resolve([{
-					'name': 'alex'
-				}, {
-					'name': 'john'
-				}]);
+				var usersClone = [];
+				// avoid two user-list directives changing the same data set!
+				angular.copy(users, usersClone);
+				defer.resolve(usersClone);
 				$log.info('end loading users ...');
 			}, 1000);
 			return defer.promise;
@@ -22,24 +30,29 @@ app.service('UserService', function($log, $timeout, $q) {
 	}
 });
 
-app.directive('userList', function(UserService) {
+app.directive('userList', function(UserService, $log) {
 	return {
 		restrict: 'E',
 		scope: {
-			load: '&load'
+			number: '=number'
 		},
-		template: '<button ng-click="load()">Load Users</button><ul> <li ng-repeat="user in users"> <input type="text" ng-model="user.name"/> </li></ul>',
+		template: '<button ng-click="load()">Load Users</button><ul ng-repeat="user in users"> <li> <input type="text" ng-model="user.name"/> </li></ul>',
 		link: function(scope, element, attributes, controller) {
-
-		},
-		controller: function($scope) {
-			$scope.load = function() {
+			scope.load = function() {
 				UserService.getAllUsers().then(function(data) {
-					$scope.users = data
-				}, function() {
+					// you can avoid the above issue here
+					/*var usersClone = [];
+					angular.copy(data.slice(0, scope.number), usersClone);
+					scope.users = usersClone;*/
 
+					scope.users = data.slice(0, scope.number);
+				}, function(reason) {
+					$log.error(reason);
 				});
 			}
+		},
+		controller: function($scope) {
+
 		}
 	}
 });
